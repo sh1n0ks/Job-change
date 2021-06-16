085f9ce7b7e0446d7e8a89916f949b74e33949c1
sh1n0ks <sh1n0ks52@icloud.com>
2021-06-17 07:33:43 +0900

    プロフィールのみハンバーガーメニューアップデート

--- js/reload.js
+++ js/reload.js
@@ -1,6 +1,14 @@
+
+// ふわっと表示
 $('head').append(
 '<style>body{display:none;}'
 );
 $(window).on("load", function() {
 $('body').delay(600).fadeIn("300");
 });
+// レスポンシブメニューの✖︎
+let icon = document.querySelector('.bar-wrapper')
+
+icon.addEventListener('click',()=>{
+  icon.classList.toggle('show-animate');
+})
